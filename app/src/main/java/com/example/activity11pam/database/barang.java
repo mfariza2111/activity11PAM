c3c1f0b8afbc0f12eeaf6e9cf85f61e9496fbd9d
mfariza2111 <farizamuhammad310@gmail.com>
2021-06-25 09:44:51 +0700

    Initial commit

---
+++ app/src/main/java/com/example/activity11pam/database/barang.java
@@ -0,0 +1,43 @@
+package com.example.activity11pam.database;
+
+import java.io.Serializable;
+
+public class barang implements Serializable{
+    private String kode;
+    private String nama;
+
+
+    public barang(){
+    }
+    public String getKode(){
+        return kode;
+    }
+
+    public void barang(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public barang(String nama) {
+        this.nama = nama;
+    }
+
+    public barang(String kode, String nama) {
+        this.kode = kode;
+        this.nama = nama;
+    }
+
+    @Override
+    public String toString() {
+        return "Barang{" +
+                "kode='" + kode + '\'' +
+                ", nama='" + nama + '\'' +
+                '}';
+    }
+
+    public void setKode(String key) {
+    }
+}
